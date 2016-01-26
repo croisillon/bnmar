03e28cd926d183eea02a6d15a43d17f87ffa04fb
Denny Laurier <diolavr@gmail.com>
2016-01-26 16:41:36 +0500

    Добавлен хэлп. Добавлена возможность управления с консоли

--- dns/02_ipton.pl
+++ dns/02_ipton.pl
@@ -1,12 +1,26 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings;
-use Socket;
+use common::sense;
+use Socket qw/inet_aton/;
+
+select STDOUT;
 $| = 1;
 
-use constant IN  => 'domains.table';
-use constant OUT => 'domains.digits.table';
+my $args = {@ARGV};
+
+my ( $resolver, $reply );
+
+if ( exists $args->{'--help'} || exists $args->{'-h'} ) {
+    &_help();
+    exit 1;
+}
+
+my $IN = $args->{'-i'} || $args->{'--input'} || 'domains.table.txt';
+
+die qq{File "$IN" is not found\n}   unless -e $IN;
+die qq{FIle "$IN" cannot be read\n} unless -r $IN;
+
+my $OUT = $args->{'-o'} || $args->{'--output'} || 'domains.digits.txt';
 
 =encoding utf8
 
@@ -24,16 +38,33 @@ Denis Lavrov C<diolavr@gmail.com>
 
 =cut
 
-open my $domain, '<', IN;
-open my $table,  '>', OUT;
+open my $in_fh, '<', $IN  or die $! . "\n";
+open my $out_fh,  '>', $OUT or die $! . "\n";
 
-for (<$domain>) {
+print "Processed...\n";
+for (<$in_fh>) {
     if ( $_ =~ m/^#/ ) { next; }
-    map { print $table ( unpack 'N', Socket::inet_aton($_) ), "\n"; }
+    map { print $out_fh ( unpack 'N', &inet_aton($_) ), "\n"; }
         split ',', $_;
 }
+print "Done...\n";
 
-close($domain);
-close($table);
+close($in_fh);
+close($out_fh);
+
+sub _help {
+    print qq{Использование: ipton.pl [-i FILE] [-o FILE]] \n},
+    q{Преобразует IP-адреса в числа },
+    qq{Результат записывается в файл.\n\n};
+
+    print sprintf("%5s, %s\t %s", '-i', '--input',  q{Входной файл (по умолчанию: domains.table.txt)}), "\n";
+    print sprintf("%5s, %s\t %s", '-o', '--output', q{Выходной файл (по умолчанию: domains.digits.txt)}), "\n";
+    print "\n";
+    print sprintf("%6s %s\t %s", '', '--help', q{Показать эту справку и выйти}), "\n";
+    print "\n";
+
+    print qq{Пример: \n},
+    q{ipton.pl -i ip.txt -o digits.txt}, "\n\n",
+}
 
 __END__
