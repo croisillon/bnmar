9dbe7c65cc1c6cc2e01a6c8b6c10dc5b60c37dcc
Denis Lavrov <diolavr@gmail.com>
2016-05-30 16:34:50 +0500

    Added console argument skip

--- run.pl
+++ run.pl
@@ -7,70 +7,90 @@ use File::Spec::Functions qw(catfile);
 use Getopt::Long 'HelpMessage';
 use FindBin qw($Bin);
 
-my ($file, $outdir);
+my ($file, $outdir, $skip);
 GetOptions( 
 	'file=s' => \$file,
-	'out=s' => \$outdir
+	'out=s' => \$outdir,
+	'skip=i' => \$skip
 ) or HelpMessage(1);
 
-my ($command, $script, $out, $input);
+my ($command, $script, $out, $input, $step_name);
 ##### ---------- STEP 1 ---------- #####
-say "01 - Filtering";
+$step_name = '01 - Filtering';
 $script = '01_netflow_filter.pl';
 $script = catfile($Bin, $script);
 $out = catfile($outdir, 'filtered.txt');
 $command = qq{$script --file $file --out $out};
-say $command;
-$input = $out;
-system $command;
+unless ( $skip ) {
+	say $step_name;
+	say $command;
+	$input = $out;
+	system $command;
+} else {
+	say "$step_name has been skipped";
+	--$skip;
+}
 
 
 my ($catalog, @agregation, $interval);
 ##### ---------- STEP 2 ---------- #####
-say "02 - Agregate";
+$step_name = '02 - Aggregating';
 $script = '02_netflow_agregate.pl';
 $script = catfile($Bin, $script);
 $catalog = 'agregation';
 $file = $input;
 
-$catalog = catfile($outdir, $catalog);
-unless ( -e $catalog ) {
-	mkdir($catalog) or die $!;
-}
+unless ( $skip ) {
+	say $step_name;
+	$catalog = catfile($outdir, $catalog);
+	unless ( -e $catalog ) {
+		mkdir($catalog) or die $!;
+	}
 
-for $interval ( 2,4,6,8,10,24 ) {
-	$out = catfile($catalog, qq{int$interval.csv});
-	$command = qq{$script --file $file --interval $interval --out $out};
-	say $command;
-	system $command;
+	for $interval ( 2,4,6,8,10,24 ) {
+		$out = catfile($catalog, qq{int$interval.csv});
+		$command = qq{$script --file $file --interval $interval --out $out};
+		say $command;
+		system $command;
 
-	push @agregation, [ $interval, $out];
+		push @agregation, [ $interval, $out];
+	}
+} else {
+	say "$step_name has been skipped";
+	--$skip;
 }
 
 ##### ---------- STEP 3 ---------- #####
-say "03 - Binning";
+$step_name = '03 - Binning';
 $script = '03_binning.pl';
 $script = catfile($Bin, $script);
 $catalog = 'binning';
 
-$catalog = catfile($outdir, $catalog);
-unless ( -e $catalog ) {
-	mkdir($catalog) or die $!;
-}
+unless ( $skip ) {
+	say $step_name;
+	$catalog = catfile($outdir, $catalog);
+	unless ( -e $catalog ) {
+		mkdir($catalog) or die $!;
+	}
 
-while ( $input = shift @agregation ) {
-	($interval, $file) = @$input;
-	$out = catfile($catalog, qq{int$interval.csv});
-	$command = qq{$script --file $file --out $out --interval $interval};
-	say $command;
-	system $command;
+	while ( $input = shift @agregation ) {
+		($interval, $file) = @$input;
+		$out = catfile($catalog, qq{int$interval.csv});
+		$command = qq{$script --file $file --out $out --interval $interval};
+		say $command;
+		system $command;
+	}
+} else {
+	say "$step_name has been skipped";
+	--$skip;
 }
 
 ##### ---------- STEP 4 ---------- #####
 my $inter = '/usr/bin/Rscript';
-say "04 - Clustering";
+$step_name = "04 - Clustering";
 $script = 'run2.r';
 $script = catfile($Bin, 'R', $script);
 $command = qq{$inter $script $outdir};
-say $command;
+say $step_name;
+# say $command;
 system $command;
